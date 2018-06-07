0d3df9a80db6da91cfa1d6312e43a849570b85c0
Jim <smart@gmail.com>
2018-06-07 16:33:46 +0800

    commit

--- java/spitter/ServletTest.java
+++ java/spitter/ServletTest.java
@@ -7,18 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
+import com.italent.controller.sys.LoginController;
+
 /**
  * Servlet implementation class ServletTest
  */
 @WebServlet("/ServletTest")
 public class ServletTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	Logger logger = Logger.getLogger(ServletTest.class);
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		System.out.println(request.getParameter("test"));
+		logger.info(request.getParameter("msg"));
 		doGet(request, response);
 	}
 
